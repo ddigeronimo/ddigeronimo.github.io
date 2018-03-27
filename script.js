d806bd17f44fae1f48363e5a1269fcedfae8362e
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-03-27 17:41:09 -0400

    Move js to external file, dawg

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('div.hidden1').fadeIn(1000).removeClass('hidden1');
+    $('div.hidden2').fadeIn(2000).removeClass('hidden2');
+    $('div.hidden3').fadeIn(3000).removeClass('hidden3');
+    $('div.hidden4').fadeIn(4000).removeClass('hidden4');
+    $('div.hidden5').fadeIn(5000).removeClass('hidden5');
+  });
