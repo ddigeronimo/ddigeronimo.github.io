0be37685948c16379d9674540b7f3ed69fe3c1e7
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-03-27 20:24:09 -0400

    Beautify code and update fade speed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // This site is the work of Dylan DiGeronimo
 $(document).ready(function () {
-    $('div.hidden1').fadeIn(1000).removeClass('hidden1');
-    $('div.hidden2').fadeIn(2000).removeClass('hidden2');
-    $('div.hidden3').fadeIn(3000).removeClass('hidden3');
-    $('div.hidden4').fadeIn(4000).removeClass('hidden4');
-    $('div.hidden5').fadeIn(5000).removeClass('hidden5');
-  });
+    $('div.hidden1').fadeIn(500).removeClass('hidden1');
+    $('div.hidden2').fadeIn(1000).removeClass('hidden2');
+    $('div.hidden3').fadeIn(1500).removeClass('hidden3');
+    $('div.hidden4').fadeIn(2000).removeClass('hidden4');
+    $('div.hidden5').fadeIn(2500).removeClass('hidden5');
+});
