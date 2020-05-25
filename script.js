6819242419c3eee37470a450ced339b8d8ba5445
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2020-05-24 21:37:53 -0400

    Remove jQuery to decrease load times

--- script.js
+++
@@ -1,7 +0,0 @@
-// This site is the work of Dylan DiGeronimo
-$(document).ready(function () {
-    $('div.hidden1').fadeIn(500).removeClass('hidden1');
-    $('div.hidden2').fadeIn(1000).removeClass('hidden2');
-    $('div.hidden3').fadeIn(1500).removeClass('hidden3');
-    $('div.hidden4').fadeIn(2000).removeClass('hidden4');
-});
